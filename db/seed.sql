a05cd3a1c2e2850a3c721d994c802e38b75eaa9d
Autumn Sheridan <animalsoa@gmail.com>
2023-07-14 08:10:40 -0700

    a commit

---
+++ db/seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO posts (post_title, post_author, post_date, post_new)
+VALUES
+  ('Random Title', 'Jane Smith', '20180805', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem.'),
+  ('Sample Post', 'Alice Johnson', '20180805', 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur.'),
+  ('Title Example', 'Bob Anderson', '20180805', 'Neque porro quisquam est, qui dolorem ipsum quia dolor.'),
+  ('Awesome Blog', 'Emily Wilson', '20180805', 'Quis autem vel eum iure reprehenderit qui in ea voluptate velit.'),
+  ('Interesting Read', 'Michael Brown', '20180805', 'Ut enim ad minima veniam, quis nostrum exercitationem ullam.'),
+  ('My Thoughts', 'Sarah Davis','20180805', 'Duis aute irure dolor in reprehenderit in voluptate velit.'),
+  ('Inspiring Story', 'David Thompson', '20180805', 'Excepteur sint occaecat cupidatat non proident, sunt in culpa.'),
+  ('Thought-provoking', 'Olivia Roberts', '20180805', 'Sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+  ('Great Content', 'Daniel Wilson', '20180805', 'Et harum quidem rerum facilis est et expedita distinctio.');
---
+++ server/server.js
@@ -0,0 +1,48 @@
+import express from "express";
+const app = express();
+import pkg from "pg";
+const { Pool } = pkg;
+
+const pool = new Pool({
+  host: "localhost",
+  port: 5432,
+  database: "postgres",
+  user: "autumn",
+});
+
+app.use(express.json()); //Middleware to parse request body
+
+// Routes
+app.get("/posts", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM posts");
+    res.status(200).send(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Delete a specific post
+app.delete("/posts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await pool.query(
+      "DELETE FROM posts WHERE id = $1 RETURNING *",
+      [id]
+    );
+    if (result.rows.length === 0) {
+      return res.status(404).send("Post not found");
+    }
+
+    res.status(200).send("Post deleted successfully");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
